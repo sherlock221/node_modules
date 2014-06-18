86cf55e5968f58f24eaf1e173490b3dd8320bb12
adjia <abjia@thinkjoy.cn>
2014-06-18 18:18:00 +0800

    Initial commit

---
+++ canvas/build/Release/.deps/Release/obj.target/canvas/src/CanvasRenderingContext2d.o.d
@@ -0,0 +1,38 @@
+cmd_Release/obj.target/canvas/src/CanvasRenderingContext2d.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DHAVE_JPEG' '-DBUILDING_NODE_EXTENSION' -I/Users/gbdai/.node-gyp/0.10.26/src -I/Users/gbdai/.node-gyp/0.10.26/deps/uv/include -I/Users/gbdai/.node-gyp/0.10.26/deps/v8/include -I../node_modules/nan -I/usr/local/include/cairo -I/usr/local/include/pixman-1 -I/usr/local/include/libpng15  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/canvas/src/CanvasRenderingContext2d.o.d.raw  -c -o Release/obj.target/canvas/src/CanvasRenderingContext2d.o ../src/CanvasRenderingContext2d.cc
+Release/obj.target/canvas/src/CanvasRenderingContext2d.o: \
+  ../src/CanvasRenderingContext2d.cc ../src/Canvas.h \
+  /Users/gbdai/.node-gyp/0.10.26/deps/v8/include/v8.h \
+  /Users/gbdai/.node-gyp/0.10.26/deps/v8/include/v8stdint.h \
+  /Users/gbdai/.node-gyp/0.10.26/src/node.h \
+  /Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv.h \
+  /Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-unix.h \
+  /Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-darwin.h \
+  /Users/gbdai/.node-gyp/0.10.26/src/node_object_wrap.h \
+  /Users/gbdai/.node-gyp/0.10.26/src/node_version.h \
+  ../node_modules/nan/nan.h \
+  /Users/gbdai/.node-gyp/0.10.26/src/node_buffer.h ../src/Point.h \
+  ../src/Image.h ../src/ImageData.h ../src/PixelArray.h \
+  ../src/CanvasRenderingContext2d.h ../src/color.h \
+  ../src/CanvasGradient.h ../src/CanvasPattern.h
+../src/CanvasRenderingContext2d.cc:
+../src/Canvas.h:
+/Users/gbdai/.node-gyp/0.10.26/deps/v8/include/v8.h:
+/Users/gbdai/.node-gyp/0.10.26/deps/v8/include/v8stdint.h:
+/Users/gbdai/.node-gyp/0.10.26/src/node.h:
+/Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv.h:
+/Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-unix.h:
+/Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv-private/ngx-queue.h:
+/Users/gbdai/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-darwin.h:
+/Users/gbdai/.node-gyp/0.10.26/src/node_object_wrap.h:
+/Users/gbdai/.node-gyp/0.10.26/src/node_version.h:
+../node_modules/nan/nan.h:
+/Users/gbdai/.node-gyp/0.10.26/src/node_buffer.h:
+../src/Point.h:
+../src/Image.h:
+../src/ImageData.h:
+../src/PixelArray.h:
+../src/CanvasRenderingContext2d.h:
+../src/color.h:
+../src/CanvasGradient.h:
+../src/CanvasPattern.h:
---
+++ canvas/build/Release/.deps/Release/obj.target/canvas/src/color.o.d
@@ -0,0 +1,4 @@
+cmd_Release/obj.target/canvas/src/color.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DHAVE_JPEG' '-DBUILDING_NODE_EXTENSION' -I/Users/gbdai/.node-gyp/0.10.26/src -I/Users/gbdai/.node-gyp/0.10.26/deps/uv/include -I/Users/gbdai/.node-gyp/0.10.26/deps/v8/include -I../node_modules/nan -I/usr/local/include/cairo -I/usr/local/include/pixman-1 -I/usr/local/include/libpng15  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/canvas/src/color.o.d.raw  -c -o Release/obj.target/canvas/src/color.o ../src/color.cc
+Release/obj.target/canvas/src/color.o: ../src/color.cc ../src/color.h
+../src/color.cc:
+../src/color.h:
